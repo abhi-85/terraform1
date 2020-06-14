cb2a189f8e100626ac5c83a7f46f6340652fdeb3
abhi-85 <61115394+abhi-85@users.noreply.github.com>
2020-06-14 19:17:11 +0530

    new

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<h1>Hello World!
-<img src="vimal sir.jpg" alt="Abhishek Sharma" width="500" height="600">
+Welcome to Abhishek Sharma page.
+<pre>
+<html>
+<body>
+<?php
+
+echo "<img src='https://abhi85.s3.ap-south-1.amazonaws.com/shailja.jpg' width='600' height='300'
+
+?>
+</body>
+</html>
+</pre>
